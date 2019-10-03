94bb51bb5cb99fac26270e626e312806adc45a6f
Mithun Prem <mithunprem19@gmail.com>
2019-10-03 15:25:42 +1000

    Defined a position object to hold the robot’s position and a commandList to hold the list of successful commands executed in CommandModule state.

--- src/Containers/CommandModule/index.js
+++ src/Containers/CommandModule/index.js
@@ -1,13 +1,25 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import CommandInput from '../../Components/CommandInput';
 import Result from '../../Components/Result';
-function CommandModule() {
-  return (
-    <Fragment>
-      <CommandInput />
-      <Result />
-    </Fragment>
-  );
-}
 
-export default CommandModule;
+export default class CommandModule extends Component {
+
+  state = {
+    commandList: [],
+    robotPosition: {
+      x: '',
+      y: '',
+      direction: ''
+    }
+  }
+
+
+  render() {
+    return (
+      <Fragment>
+        <CommandInput/>
+        <Result />
+      </Fragment>
+    );
+  }
+}
