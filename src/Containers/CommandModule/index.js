b9150e0284106304fd3da80fddfded770334f401
Mithun Prem <mithunprem19@gmail.com>
2019-10-03 17:18:19 +1000

    Invoked the helper function to execute the moves when the user clicks on the button.

--- src/Containers/CommandModule/index.js
+++ src/Containers/CommandModule/index.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import CommandInput from '../../Components/CommandInput';
 import Result from '../../Components/Result';
+import processCommand from '../../Utils/commandProcessor';
 
 export default class CommandModule extends Component {
 
@@ -13,11 +14,20 @@ export default class CommandModule extends Component {
     }
   }
 
+  executeCommand = command => {
+    const { commandList, robotPosition } = this.state;
+    const updatedResult = processCommand(command, commandList, robotPosition);
+    this.setState({
+      commandList: updatedResult.commandList,
+      robotPosition: updatedResult.position
+    })
+
+  }
 
   render() {
     return (
       <Fragment>
-        <CommandInput/>
+        <CommandInput executeCommand={this.executeCommand}/>
         <Result />
       </Fragment>
     );
