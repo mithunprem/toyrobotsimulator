40e9797d49d36ba36ab175c1ddb04a813dacad49
Mithun Prem <mithunprem19@gmail.com>
2019-10-03 17:17:47 +1000

    Developed the outline for the basic computation logic to handle the various commands.

---
+++ src/Utils/commandProcessor.js
@@ -0,0 +1,84 @@
+const PLACE_DELIMITER = ' ';
+const POSITION_DELIMITER = ',';
+
+const processCommand = (command, commandList, position) => {
+
+  if (command.startsWith('place')) {
+    const [x, y, direction] = positionCoordinates(command);
+
+    if (x<5 && y<5) {
+      position.x = Number(x);
+      position.y = Number(y);
+      position.direction = direction;
+      commandList.push(command);
+    }
+  } else {
+    let { x, direction } = position;
+
+    if (x !== '') {
+      switch(command) {
+        case 'move':
+          position = executeMove(position);
+          commandList.push(command); // Should push only for valid executed commands.
+          break;
+        case 'left':
+        case 'right':
+          direction = executePositionChange(command, direction);
+          position.direction = direction;
+          commandList.push(command);
+          break;
+        default:
+      }
+    }
+  }
+  return { commandList, position };
+}
+
+const positionCoordinates = command => {
+  const position = command.split(PLACE_DELIMITER)[1];
+  return position.split(POSITION_DELIMITER);
+}
+
+const executeMove = ({ x, y, direction }) => {
+  switch (direction) {
+    case 'north':
+      if ( y < 4 ) y = y+1;
+      break;
+    case 'south':
+      if ( y > 0 ) y = y-1;
+      break;
+    case 'east':
+      if ( x < 4 ) x = x+1;
+      break;
+    case 'west':
+      if ( x > 0 ) x = x-1;
+      break;
+    default:
+      break;
+  }
+
+  return { x, y, direction };
+}
+
+const executePositionChange = (command, direction) => {
+  switch(direction) {
+    case 'north':
+      direction = (command === 'left' ? 'west' : 'east');
+      break;
+    case 'south':
+      direction = (command === 'left' ? 'east' : 'west');
+      break;
+    case 'east':
+      direction = (command === 'left' ? 'north' : 'south');
+      break;
+    case 'west':
+      direction = (command === 'left' ? 'south' : 'north');
+      break;
+    default:
+  }
+
+  return direction;
+}
+
+
+export default processCommand;
