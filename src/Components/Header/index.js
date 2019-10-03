bced5a083a369300c5bef74575a15130323e9671
Mithun Prem <mithunprem19@gmail.com>
2019-10-03 11:21:26 +1000

    Added a header component for the app.

---
+++ src/Components/Header/index.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+import './header.scss';
+
+const Header = () => {
+  return (
+    <Fragment>
+      <header className="toy-robot-simulator-header">
+        <h5 className="ml-3 header-text">Toy Robot Simulator</h5>
+      </header>
+    </Fragment>
+  );
+}
+
+export default Header;
