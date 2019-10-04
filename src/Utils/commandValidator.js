c783875912dc0760fb5e93017aefc4abf978acde
Mithun Prem <mithunprem19@gmail.com>
2019-10-04 10:21:17 +1000

    Define the board dimension as a constant and use it in the validator. This will make it easier to support any board sizes in the future.

--- src/Utils/commandValidator.js
+++ src/Utils/commandValidator.js
@@ -1,3 +1,5 @@
+import { BOARD_DIMENSION } from '../Constants';
+
 const validateCommand = command => {
   let isValidCommand = false;
 
@@ -6,17 +8,24 @@ const validateCommand = command => {
   command = command.toLowerCase();
 
   // For PLACE commands, validate the command with a regex to check if
-  // the command has valid coordinates and direction.
-  if (command.startsWith('place')) {
-    isValidCommand = placeCommandValidationRegex.test(command);
-  } else if (validCommandsList.indexOf(command) !== -1) {
+  // the command has valid coordinates and direction. For other commands, just
+  // check if the passed in command is part of the valid commands list.
+  if (validCommandsList.indexOf(command) !== -1 || placeCommandValidationRegex.test(command)) {
     isValidCommand = true;
   }
-  
+
   return isValidCommand;
 }
 
-const placeCommandValidationRegex = RegExp(/place[ ][0-4][,][0-4][,](north|south|east|west)$/);
+// For a 5*5 board, the maximum x or y coordinate can be 4.
+const maxCoordinate = Number(BOARD_DIMENSION) - 1;
+
+// Validation regex for PLACE command. The place command should be in the
+// format PLACE x,y,position. eg: PLACE 0,0,NORTH.
+const placeCommandValidationRegex =
+  RegExp(`place[ ][0-${maxCoordinate}][,][0-${maxCoordinate}][,](north|south|east|west)$`);
+
+// List of valid commands excluding the place command.
 const validCommandsList = ['move', 'left', 'right', 'report'];
 
 export default validateCommand;
