8256ba0b1ef8fb643e7c3300b512e0d720ccdf50
Mithun Prem <mithunprem19@gmail.com>
2019-10-04 20:13:47 +1000

    Added a tooltip in Input component.

--- src/Components/Input/index.js
+++ src/Components/Input/index.js
@@ -1,29 +1,37 @@
 import React, { Fragment } from 'react';
+import { Tooltip } from 'reactstrap';
 import PropTypes from 'prop-types';
 import './input.scss';
 
-const Input = ({ type = "text", label, placeholder, className, value, onChange = () => { } }) => {
+const Input = ({ type = "text", name, label, placeholder, showError, value, errorMessage, onChange = () => { } }) => {
+
+  const errorClassName = showError ? 'has-error' : '';
+
   return (
     <Fragment>
       <label htmlFor={placeholder}>{label}</label>
       <br/>
       <input
-        className={className}
+        id={name}
+        className={`command-input ${errorClassName}`}
         placeholder={placeholder}
         type={type}
         value={value}
         onChange={onChange}
       />
+      <Tooltip placement="bottom" target={name} fade={false} isOpen={showError}>{errorMessage}</Tooltip>
     </Fragment>
   )
 }
 
 Input.propTypes = {
+  name: PropTypes.string,
   label: PropTypes.string,
   type: PropTypes.string,
   placeholder: PropTypes.string,
-  className: PropTypes.string,
+  showError: PropTypes.bool,
   value: PropTypes.string,
+  errorMessage: PropTypes.string,
   onChange: PropTypes.func
 }
 
