0554829a896b9a95059c2c74e605eef7ad8c3b03
Mithun Prem <mithunprem19@gmail.com>
2019-10-04 19:18:59 +1000

    Added a reset button to take the robot back off the table.

--- src/Containers/CommandModule/index.js
+++ src/Containers/CommandModule/index.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react';
 import CommandInput from '../../Components/CommandInput';
 import CommandList from '../../Components/CommandList';
 import Report from '../../Components/Report';
+import Button from '../../Components/Button';
 import processCommand from '../../Utils/commandProcessor';
 import './commandModule.scss';
 
@@ -25,6 +26,18 @@ export default class CommandModule extends Component {
     );
   }
 
+  resetRobot = () => {
+    this.setState({
+      commandList: [],
+      robotPosition: {
+        x: -1,
+        y: -1,
+        direction: ''
+      },
+      shouldReportRobotStatus: false
+    })
+  }
+
   render() {
     const { commandList, robotPosition, shouldReportRobotStatus } = this.state;
 
@@ -37,7 +50,15 @@ export default class CommandModule extends Component {
           }
         </div>
         <hr />
-        <CommandList commandList={commandList} />
+        {
+          (commandList.length > 0) ? (
+            <Fragment>
+              <Button className="ml-3 mb-3 btn btn-light"
+                  size="sm" label="Reset the Robot" onClick={this.resetRobot} />
+              <CommandList commandList={commandList} />
+            </Fragment>
+          ) : ''
+        }
       </Fragment>
     );
   }
