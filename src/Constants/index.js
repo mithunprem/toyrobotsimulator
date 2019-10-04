4674edfe3194c083873913f279113832cb2f08fd
Mithun Prem <mithunprem19@gmail.com>
2019-10-04 12:42:39 +1000

    Declared a new constant MAX_COORDINATE in the constants file which will denote the maximum limit to which a robot can move in x or y direction.

--- src/Constants/index.js
+++ src/Constants/index.js
@@ -1,4 +1,8 @@
-export const BOARD_DIMENSION = 5;
+// Board dimensions. 5 will indicate a 5*5 board.
+const BOARD_DIMENSION = 5;
+
+// For a 5*5 board, the maximum x or y coordinate can be 4.
+export const MAX_COORDINATE = Number(BOARD_DIMENSION) - 1;
 
 // Delimiters in the PLACE command.
 export const PLACE_DELIMITER = ' ';
--- src/Utils/commandValidator.js
+++ src/Utils/commandValidator.js
@@ -1,4 +1,4 @@
-import { BOARD_DIMENSION } from '../Constants';
+import { MAX_COORDINATE } from '../Constants';
 
 const validateCommand = command => {
   let isValidCommand = false;
@@ -7,9 +7,11 @@ const validateCommand = command => {
   // the same for any case input.
   command = command.toLowerCase();
 
-  // For PLACE commands, validate the command with a regex to check if
-  // the command has valid coordinates and direction. For other commands, just
-  // check if the passed in command is part of the valid commands list.
+  /*
+    For PLACE commands, validate the command with a regex to check if
+    the command has valid coordinates and direction. For other commands, just
+    check if the passed in command is part of the valid commands list.
+  */
   if (validCommandsList.indexOf(command) !== -1 || placeCommandValidationRegex.test(command)) {
     isValidCommand = true;
   }
@@ -17,13 +19,10 @@ const validateCommand = command => {
   return isValidCommand;
 }
 
-// For a 5*5 board, the maximum x or y coordinate can be 4.
-const maxCoordinate = Number(BOARD_DIMENSION) - 1;
-
 // Validation regex for PLACE command. The place command should be in the
 // format PLACE x,y,position. eg: PLACE 0,0,NORTH.
 const placeCommandValidationRegex =
-  RegExp(`place[ ][0-${maxCoordinate}][,][0-${maxCoordinate}][,](north|south|east|west)$`);
+  RegExp(`place[ ][0-${MAX_COORDINATE}][,][0-${MAX_COORDINATE}][,](north|south|east|west)$`);
 
 // List of valid commands excluding the place command.
 const validCommandsList = ['move', 'left', 'right', 'report'];
