9fbfdbfa37a2a9a4e1b12ead88a8eaaa6bb6d805
Mithun Prem <mithunprem19@gmail.com>
2019-10-06 12:30:31 +1100

    Removed the unused button.scss.

--- src/Components/Button/index.js
+++ src/Components/Button/index.js
@@ -1,6 +1,5 @@
 import React, { Fragment  } from 'react';
 import PropTypes from 'prop-types';
-import './button.scss';
 
 const Button = ({ className, label, disabled, onClick = () => { } }) => {
   return (
