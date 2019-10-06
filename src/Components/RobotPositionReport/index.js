866b650860d0da2c69a469e7f4c6c6d221e053ea
Mithun Prem <mithunprem19@gmail.com>
2019-10-06 13:43:47 +1100

    Renamed the Report component to RobotPositionReport.

--- src/Components/RobotPositionReport/index.js
+++ src/Components/RobotPositionReport/index.js
@@ -1,15 +1,15 @@
 import React, { Fragment } from 'react';
 
-const Report = ({ robotPosition }) => {
+const RobotPositionReport = ({ robotPosition }) => {
 
   const { x, y, direction } = robotPosition;
   return (
     <Fragment>
       <div className="report">
-        <span className="ml-3">The current position is: {x}, {y}, {direction.toUpperCase()}</span>
+        <p className="ml-3">The current position is: {x}, {y}, {direction.toUpperCase()}</p>
       </div>
     </Fragment>
   );
 }
 
-export default Report;
+export default RobotPositionReport;
