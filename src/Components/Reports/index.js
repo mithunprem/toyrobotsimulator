29dd0cd10d91e5ed5ab59a4191984ecc090fab93
Mithun Prem <mithunprem19@gmail.com>
2019-10-06 14:13:36 +1100

    Refactored the Reports component.

--- src/Components/Reports/index.js
+++ src/Components/Reports/index.js
@@ -1,22 +1,21 @@
 import React, { Fragment } from 'react';
 import CommandList from '../../Components/CommandList';
 import RobotPositionReport from '../../Components/RobotPositionReport';
+import './reports.scss';
 
 const Reports = ({ reportRobotStatus, robotPosition, commandList }) => {
 
   return (
     <Fragment>
-    {
-      reportRobotStatus ? <RobotPositionReport robotPosition={robotPosition} /> : ''
-    }
-    {
-      (commandList.length > 0) ? (
-        <Fragment>
-          <CommandList commandList={commandList} />
-        </Fragment>
-      ) : ''
-    }
-  </Fragment>
+      <div className="reports m-3">
+        { reportRobotStatus ?
+          <RobotPositionReport robotPosition={robotPosition} /> : ''
+        }
+        { commandList.length > 0 ?
+          <CommandList commandList={commandList} /> : ''
+        }
+      </div>
+    </Fragment>
   );
 }
 
