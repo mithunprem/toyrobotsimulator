4fe2bba0d980a087d0baa91e8d90a4feb1ae5290
Mithun Prem <mithunprem19@gmail.com>
2019-10-06 14:14:02 +1100

    Displayed the report in a bootstrap alert view.

--- src/Components/RobotPositionReport/index.js
+++ src/Components/RobotPositionReport/index.js
@@ -3,10 +3,11 @@ import React, { Fragment } from 'react';
 const RobotPositionReport = ({ robotPosition }) => {
 
   const { x, y, direction } = robotPosition;
+
   return (
     <Fragment>
-      <div className="report">
-        <p className="ml-3">The current position is: {x}, {y}, {direction.toUpperCase()}</p>
+      <div className="report alert alert-info">
+        <span>Robot's current position is: {x}, {y}, {direction.toUpperCase()}</span>
       </div>
     </Fragment>
   );
